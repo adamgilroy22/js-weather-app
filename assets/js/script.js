743af91b619440773efd25994f30b8f4b3207755
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-11 17:36:45 +0000

    Add search function and set default to Cork

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 var key = config.WEATHER_API_KEY;
 
 let weather = {
-    fetchWeather: function(city) {
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&&appid=" + key
-        ).then((response) => response.json())
-        .then((data) => this.displayWeather(data));
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&&appid=" + key
+        ).then((response) => {
+            if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found.");
+            }
+            return response.json();
+        })
+            .then((data) => this.displayWeather(data));
     },
 
-    displayWeather: function(data) {
+    displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
@@ -21,5 +27,23 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    }
+    },
+
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("Cork");
