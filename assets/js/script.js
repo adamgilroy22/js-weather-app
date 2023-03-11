ab3ce8fefd2d2cdc041e330040811ca860727b3f
Adam Gilroy <adamgilroy22@gmail.com>
2023-03-11 17:12:40 +0000

    Create fetchWeather function using weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 var key = config.WEATHER_API_KEY;
 
 let weather = {
-    "apiKey": key
-}
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&&appid=" + key
+        ).then((response) => response.json())
+        .then((data) => console.log(data))
+    },
+
+    displayWeather: function(data) {
+
+    }
+};
